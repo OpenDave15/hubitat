213f30d8d2737001d10b24529bb819a378fc3fab
Mirco Caramori <mcaramori@padus.com>
2019-12-13 11:05:01 -0800

    first implementation

--- ambientweather/app.groovy
+++ ambientweather/app.groovy
@@ -1,144 +1,180 @@
-definition(name: "Ambient Weather API", namespace: "CordMaster", author: "Alden Howard", description: "A simple api for providing ambient weather access", iconUrl: "", iconX2Url: "");
+/**
+ * IMPORT URL: https://raw.githubusercontent.com/mircolino/hubitat/master/ambientweather/app.groovy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the License.
+ *
+ * Credits:
+ *
+ * Alden Howard (https://github.com/thoward1234/Hubitat-Ambient-Weather): initial implementation
+ *
+*/
+
+definition(name: "Ambient Weather Station", namespace: "mircolino", author: "Mirco Caramori", description: "API to access ambientweather.net", iconUrl: "", iconX2Url: "");
+
+// ------------------------------------------------------------
 
 preferences {
-    page(name: "page1", title: "Log In", nextPage: "page2", uninstall: true) {
-        section {
-            input(name: "applicationKey", title: "Application Key", type: "text", required: true);
-            input(name: "apiKey", title: "API Key", type: "text", required: true);
-        }
+  page(name: "page1", title: "Log In", nextPage: "page2", uninstall: true) {
+    section {
+      input(name: "applicationKey", title: "Application Key", type: "text", required: true);
+      input(name: "apiKey", title: "API Key", type: "text", required: true);
     }
-    
-    page(name: "page2");
-    page(name: "page3");
+  }
+
+  page(name: "page2");
+  page(name: "page3");
 }
+
+// ------------------------------------------------------------
 
 def page2() {
-    def stations = [];
-    def stationMacs = [];
-    try {
-        stations = getStations();
-        
-        stations.each { stationMacs << it.macAddress };
-    } catch(groovyx.net.http.HttpResponseException e) {
-        //then unauthorized
-        return dynamicPage(name: "page2", title: "Error", nextPage: "page1", uninstall: true) {
-            section {
-                paragraph("There was an error authorizing you. Please try again.");
-            }
-        }
+  def stations = [];
+  def stationMacs = [];
+  try {
+    stations = getStations();
+
+    stations.each { stationMacs << it.macAddress };
+  }
+  catch(groovyx.net.http.HttpResponseException e) {
+    // Unauthorized
+    return dynamicPage(name: "page2", title: "Error", nextPage: "page1", uninstall: true) {
+      section {
+        paragraph("There was an error authorizing you. Please try again.");
+      }
     }
-    
-   	log.debug("Got stations: " + stations);
-    
-	return dynamicPage(name: "page2", title: "Select Station", nextPage: "page3", uninstall: true) {
-		section {
-			input(name: "station", title: "Station", type: "enum", options: stationMacs, required: true);
-            input(name: "refreshInterval", title: "Refresh Interval (in minutes)", type: "number", range: "1..3600", defaultValue: 1, required: true);
-		}
-	}
+  }
+
+  log.debug("Got stations: " + stations);
+
+  return dynamicPage(name: "page2", title: "Select Station", nextPage: "page3", uninstall: true) {
+    section {
+      input(name: "station", title: "Station", type: "enum", options: stationMacs, required: true);
+      input(name: "refreshInterval", title: "Refresh Interval (in minutes)", type: "number", range: "1..3600", defaultValue: 1, required: true);
+    }
+  }
 }
+
+// ------------------------------------------------------------
 
 def page3() {
-    dynamicPage(name: "page3", title: "Confirm Settings", install: true, uninstall: true) {
-        section {
-            paragraph("Selected station: $station");
-            paragraph("Refresh interval: $refreshInterval minute(s)");
-        }
-        
-        section {
-            paragraph("Press done to finish");
-        }
+  dynamicPage(name: "page3", title: "Confirm Settings", install: true, uninstall: true) {
+    section {
+      paragraph("Selected station: $station");
+      paragraph("Refresh interval: $refreshInterval minute(s)");
     }
+
+    section {
+      paragraph("Press done to finish");
+    }
+  }
 }
 
-//lifecycle functions
+// Lifecycle functions ----------------------------------------
+
 def installed() {
-    log.debug("Installed");
-    
-    addDevice();
-    
-    initialize();
-    
-    runEvery5Minutes(fetchNewWeather);
+  log.debug("Installed");
+
+  addDevices();
+  initialize();
+  runEvery5Minutes(fetchNewWeather);
 }
+
+// ------------------------------------------------------------
 
 def updated() {
-    log.debug("Updated");
-    
-    unsubscribe();
-    unschedule();
-    installed();
-    initialize(); 
+  log.debug("Updated");
+
+  unsubscribe();
+  unschedule();
+  installed();
+  initialize();
 }
+
+// ------------------------------------------------------------
 
 def initialize() {
-    fetchNewWeather();
-    
-    //chron schedule, refreshInterval is int
-    def m = refreshInterval;
-    def h = Math.floor(m / 60);
-    m -= h * 60;
-    
-    m = m == 0 ? "*" : "0/" + m.toInteger();
-    h = h == 0 ? "*" : "0/" + h.toInteger();
-    
-    log.debug("CRON schedule with m: $m and h: $h");
-    
-    schedule("0 $m $h * * ? *", fetchNewWeather);
+  fetchNewWeather();
+
+  // Chron schedule, refreshInterval is int
+  def m = refreshInterval;
+  def h = Math.floor(m / 60);
+  m -= h * 60;
+
+  m = m == 0 ? "*" : "0/" + m.toInteger();
+  h = h == 0 ? "*" : "0/" + h.toInteger();
+
+  log.debug("CRON schedule with m: $m and h: $h");
+
+  schedule("0 $m $h * * ? *", fetchNewWeather);
 }
 
-//children
-def addDevice() {
-	
-    addChildDevice("CordMaster", "Ambient Weather Device", "AWTILE-$station", null, [completedSetup: true]);
+// Children ---------------------------------------------------
+
+def addDevices() {
+  addChildDevice("mircolino", "AmbientWeather Outdoor Sensor", "AWTILE-$station", null, [completedSetup: true]);
+  addChildDevice("mircolino", "AmbientWeather Indoor Sensor", "AWTILE-$station", null, [completedSetup: true]);
 }
 
-//fetch functions
+// fetch functions --------------------------------------------
+
 def getStations() throws groovyx.net.http.HttpResponseException {
-    def data = [];
-    
-    def params = [
-        uri: "https://api.ambientweather.net/",
-        path: "/v1/devices",
-        query: [applicationKey: applicationKey, apiKey: apiKey]
-    ];
-    
-    requestData("/v1/devices", [applicationKey: applicationKey, apiKey: apiKey]) { response ->
-        data = response.data;
-    };
-        
-    return data;
+  def data = [];
+
+  def params = [
+    uri: "https://api.ambientweather.net/",
+    path: "/v1/devices",
+    query: [applicationKey: applicationKey, apiKey: apiKey]
+  ];
+
+  requestData("/v1/devices", [applicationKey: applicationKey, apiKey: apiKey]) { response ->
+    data = response.data;
+  };
+
+  return data;
 }
+
+// ------------------------------------------------------------
 
 def getWeather() throws groovyx.net.http.HttpResponseException {
-    def data = [];
-    
-    requestData("/v1/devices/$station", [applicationKey: applicationKey, apiKey: apiKey, limit: 1]) { response ->
-        data = response.data;
-    };
-        
-	return data[0];
+  def data = [];
+
+  requestData("/v1/devices/$station", [applicationKey: applicationKey, apiKey: apiKey, limit: 1]) { response ->
+    data = response.data;
+  };
+
+  return data[0];
 }
+
+// ------------------------------------------------------------
 
 def requestData(path, query, code) {
-    def params = [
-        uri: "https://api.ambientweather.net/",
-        path: path,
-        query: query
-    ];
-    
-    httpGet(params) { response ->
-        code(response);
-    };
+  def params = [
+    uri: "https://api.ambientweather.net/",
+    path: path,
+    query: query
+  ];
+
+  httpGet(params) { response ->
+    code(response);
+  };
 }
 
-//loop
+// loop -------------------------------------------------------
+
 def fetchNewWeather() {
-        
-    def weather = getWeather();
-    
-    //log.debug("Weather: " + weather);
-	
-	childDevices[0].setWeather(weather);
+  def weather = getWeather();
+
+  //log.debug("Weather: " + weather);
+
+  childDevices[0].setWeather(weather);
+  childDevices[1].setWeather(weather);
 }
 
+// ------------------------------------------------------------
---
+++ ambientweather/dev-indoor.groovy
@@ -0,0 +1,181 @@
+/**
+ * IMPORT URL: https://raw.githubusercontent.com/mircolino/hubitat/master/ambientweather/dev-indoor.groovy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the License.
+ *
+ * Credits:
+ *
+ * Scott Grayban (https://gitlab.borgnet.us:8443/sgrayban/Hubitat-Ambient-Weather-Improved): improvements
+ * Alden Howard (https://github.com/thoward1234/Hubitat-Ambient-Weather): initial implementation
+ *
+*/
+
+metadata {
+  definition(name: "Ambient Weather Indoor Sensor", namespace: "mircolino", author: "Mirco Caramori") {
+    capability "Temperature Measurement"
+    capability "Relative Humidity Measurement"
+    capability "Pressure Measurement"
+    capability "Illuminance Measurement"
+    capability "Refresh"
+    capability "Sensor"
+    capability "Actuator"
+
+    // Current Conditions
+    attribute "weather", "string"
+    attribute "weatherIcon", "string"
+    attribute "dewPoint", "number"
+    attribute "comfort", "number"
+    attribute "feelsLike", "number"
+
+    // Indoor Conditions
+    attribute "pressure_indoor", "number"
+
+    // Precipitation
+    attribute "precip_today", "number"
+    attribute "precip_1hr", "number"
+    attribute "hourlyrainin", "number"
+    attribute "weeklyrainin", "number"
+    attribute "monthlyrainin", "number"
+    attribute "totalrainin", "number"
+    attribute "lastRain", "string"
+
+    // Wind
+    attribute "wind", "number"
+    attribute "wind_gust", "number"
+    attribute "maxdailygust", "number"
+    attribute "wind_degree", "number"
+    attribute "wind_dir", "string"
+    attribute "wind_direction", "string"
+
+    // Light
+    attribute "solarradiation", "number"
+    attribute "uv", "number"
+  }
+
+  preferences {
+    section("Preferences") {
+      input "showLogs", "bool", required: false, title: "Show Debug Logs?", defaultValue: false
+    }
+  }
+}
+
+// ------------------------------------------------------------
+
+private logger(type, msg) {
+  if (type && msg && settings?.showLogs) {
+    log."${type}" "${msg}"
+  }
+}
+
+// ------------------------------------------------------------
+
+def refresh() {
+  parent.fetchNewWeather();
+}
+
+// ------------------------------------------------------------
+
+def setWeather(weather) {
+  logger("debug", "Weather: "+weather);
+
+  // Set Temperature
+  sendEvent(name: "temperature", value: weather.tempinf, unit: '°F', isStateChange: true);
+
+  // Set Humidity
+  sendEvent(name: "humidity", value: weather.humidityin, unit: '%', isStateChange: true);
+
+  // Set DewPoint
+  sendEvent(name: "dewPoint", value: weather.dewPoint, unit:'°F', isStateChange: true);
+
+  // Set Comfort Level
+  float temp = 0.0;
+
+  temp = (weather.dewPoint - 35);
+  if (temp <= 0) {
+    temp = 0.0;
+  }
+  else if (temp >= 40.0) {
+    temp = 100.0;
+  }
+  else {
+    temp = (temp/40.0)*100.0;
+  }
+  temp = temp.round(1);
+  sendEvent(name: "comfort", value: temp, isStateChange: true);
+
+  // Set Barometric Pressure
+  sendEvent(name: "pressure", value: weather.baromrelin, unit: 'in', isStateChange: true);
+
+  // Set Indoor Barometric Pressure
+  sendEvent(name: "pressure_indoor", value: weather.baromabsin, unit: 'in', isStateChange: true);
+
+  // Set Feels Like Temperature
+  sendEvent(name: "feelsLike", value: weather.feelsLike, unit: '°F', isStateChange: true);
+
+    // Rain
+  sendEvent(name: "precip_today", value: weather.dailyrainin, unit: 'in', isStateChange: true);
+  sendEvent(name: "precip_1hr", value: weather.hourlyrainin, unit: 'in', isStateChange: true);
+  sendEvent(name: "weeklyrainin", value: weather.weeklyrainin, unit: 'in', isStateChange: true);
+  sendEvent(name: "monthlyrainin", value: weather.monthlyrainin, unit: 'in', isStateChange: true);
+  sendEvent(name: "totalrainin", value: weather.totalrainin, unit: 'in', isStateChange: true);
+  sendEvent(name: "hourlyrainin", value: weather.hourlyrainin, unit: 'in', isStateChange: true);
+  sendEvent(name: "lastRain",  value: weather.lastRain, isStateChange: true);
+
+  // Wind
+  sendEvent(name: "wind", value: weather.windspeedmph, unit: 'mph', isStateChange: true);
+  sendEvent(name: "wind_gust", value: weather.windgustmph, unit: 'mph', isStateChange: true);
+  sendEvent(name: "maxdailygust", value: weather.maxdailygust, unit: 'mph', isStateChange: true);
+  sendEvent(name: "wind_degree", value: weather.winddir, unit: '°', isStateChange: true);
+
+  temp = weather.winddir
+  if (temp < 22.5) {
+    sendEvent(name:  "wind_direction", value: "North", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "N", isStateChange: true);
+  }
+  else if (temp < 67.5) {
+    sendEvent(name:  "wind_direction", value: "Northeast", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "NE", isStateChange: true);
+  }
+  else if (temp < 112.5) {
+    sendEvent(name: "wind_direction", value: "East", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "E", isStateChange: true);
+  }
+  else if (temp < 157.5) {
+    sendEvent(name: "wind_direction", value: "Southeast", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "SE", isStateChange: true);
+  }
+  else if (temp < 202.5) {
+    sendEvent(name: "wind_direction", value: "South", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "S", isStateChange: true);
+  }
+  else if (temp < 247.5) {
+    sendEvent(name: "wind_direction", value: "Southwest", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "SW", isStateChange: true);
+  }
+  else if (temp < 292.5) {
+    sendEvent(name: "wind_direction", value: "West", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "W", isStateChange: true);
+  }
+  else if (temp < 337.5) {
+    sendEvent(name: "wind_direction", value: "Northwest", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "NW", isStateChange: true);
+  }
+  else {
+    sendEvent(name:  "wind_direction", value: "North", isStateChange: true);
+    sendEvent(name:  "wind_dir", value: "N", isStateChange: true);
+  }
+
+  // UV and Light
+  sendEvent(name: "solarradiation", value: weather.solarradiation, isStateChange: true);
+  sendEvent(name: "illuminance", value: weather.solarradiation, isStateChange: true);
+  sendEvent(name: "uv", value: weather.uv, isStateChange: true);
+}
+
+// ------------------------------------------------------------
